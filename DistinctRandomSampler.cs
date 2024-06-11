047115e97eb931b4c6136b22fce979b3e74f4722
unionofonions <f181213102@ktun.edu.tr>
2024-06-11 03:21:51 +0300

    Add project files.

---
+++ DistinctRandomSampler.cs
@@ -0,0 +1,89 @@
+﻿#nullable enable
+
+namespace Extensions
+{
+	using System;
+	using System.Runtime.InteropServices;
+
+	/// <summary>
+	/// Provides random selection of elements from a collection without repeating the last N elements.
+	/// <br>Does not store any additional collection, and has O(1) complexity for sampling.</br>
+	/// </summary>
+	/// <typeparam name="T">Type of the elements in the collection.</typeparam>
+	public class DistinctRandomSampler<T>
+	{
+		private readonly T?[] m_Values;
+		private readonly int m_SkipThreshold;
+		private readonly Random m_Random;
+		private int m_ThresholdIndex;
+		private int m_SkipIndex;
+
+		// Exposed for testing
+		public int SkipThreshold
+		{
+			get => m_SkipThreshold;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DistinctRandomSampler{T}"/> class.
+		/// </summary>
+		/// <param name="values">Array of elements to sample from.</param>
+		/// <param name="skipThreshold">Number of elements to avoid repeating.</param>
+		/// <param name="random">Optional random number generator instance.</param>
+		/// <exception cref="ArgumentNullException"></exception>
+		public DistinctRandomSampler(T?[] values, int skipThreshold, [Optional] Random? random)
+		{
+			if (values == null)
+			{
+				throw new ArgumentNullException(nameof(values));
+			}
+
+			// Ensure skip threshold is between 0 and size - 1
+			if (skipThreshold < 0)
+			{
+				skipThreshold = 0;
+			}
+			else if (skipThreshold >= values.Length)
+			{
+				skipThreshold = Math.Max(values.Length - 1, 0);
+			}
+
+			m_Values = values;
+			m_SkipThreshold = skipThreshold;
+			m_ThresholdIndex = 0;
+			m_SkipIndex = 0;
+#if NET6_0_OR_GREATER
+			m_Random ??= Random.Shared;
+#else
+			m_Random ??= new();
+#endif
+		}
+
+		/// <summary>
+		/// Returns he next random element from the collection, ensuring it is not one of the last N elements.
+		/// </summary>
+		/// <returns>The next random element.</returns>
+		public T? Next()
+		{
+			// Since the initial value of threshold index is 0, all elements have an equal chance of being selected.
+			var index = m_Random.Next(m_ThresholdIndex, m_Values.Length);
+			var ret = m_Values[index];
+
+			m_Values[index] = m_Values[m_SkipIndex];
+			m_Values[m_SkipIndex] = ret;
+
+			// Update the threshold index until it reaches the skip threshold
+			if (m_ThresholdIndex < m_SkipThreshold)
+			{
+				++m_ThresholdIndex;
+			}
+
+			// Increment and reset the skip index as necessary
+			if (++m_SkipIndex >= m_SkipThreshold)
+			{
+				m_SkipIndex = 0;
+			}
+			return ret;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿#nullable enable
+
+namespace Example
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+
+	public class Program
+	{
+		static private void Main()
+		{
+			var values = new string[] { "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth" };
+			var skipThreshold = 6;
+			var sampler = new Extensions.DistinctRandomSampler<string>(values, skipThreshold);
+			const int iterations = 10_000_000;
+
+
+			// Test repetition
+			Console.WriteLine("Repetition test started...");
+			var selected = new Queue<string?>();
+			for (var i = 0; i < iterations; ++i)
+			{
+				var curr = sampler.Next();
+				if (selected.Contains(curr))
+				{
+					Console.WriteLine("TEST FAILED");
+					return;
+				}
+				// The sampler might alter the skip threshold, so we use the property instead of the local variable
+				if (selected.Count >= sampler.SkipThreshold)
+				{
+					selected.Dequeue();
+				}
+				selected.Enqueue(curr);
+			}
+			Console.WriteLine("TEST PASSED\n");
+
+
+			// Speed test
+			Console.WriteLine("Speed test started...");
+			var stopwatch = Stopwatch.StartNew();
+			for (var i = 0; i < iterations; ++i)
+			{
+				_ = sampler.Next();
+			}
+			stopwatch.Stop();
+			Console.WriteLine($"{iterations:N0} iterations done in {stopwatch.ElapsedMilliseconds} ms.");
+
+
+			// Print some samples
+			bool print = true;
+			if (print)
+			{
+				const int printCount = 20;
+				Console.WriteLine($"\n\nFirst {printCount} samples:\n");
+				for (var i = 0; i < printCount; ++i)
+				{
+					Console.WriteLine(sampler.Next());
+				}
+			}
+		}
+	}
+}
